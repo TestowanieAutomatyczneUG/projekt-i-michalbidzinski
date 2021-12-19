ad6ff64ccaf4d9801956ca2f287830f65ab482c6
Michał Bidziński <michal.bidzinski1337@gmail.com>
2021-12-20 00:40:30 +0100

    dodano testy, lecz test dla zdania nie przechodzi

--- src/sample/Ceasar.py
+++ src/sample/Ceasar.py
@@ -1,6 +1,6 @@
 class Ceasar:
     def __init__(self):
-        self.alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        self.alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ]
     def encrypt(self, text):
         cipher = ''
         for i in text:
--- tests/test_ceasar_pyhamcrest.py
+++ tests/test_ceasar_pyhamcrest.py
@@ -8,5 +8,18 @@ class TestCeasar(unittest.TestCase):
         assert_that(self.temp.encrypt('a'), equal_to('d'))
     def test_ceasar_word(self):
         assert_that(self.temp.encrypt('VENI'), equal_to('YHQL'))
+    def test_ceasar_word_lowercase(self):
+        assert_that(self.temp.encrypt('veni'), equal_to('yhql'))
     def test_ceasar_last_3_letters(self):
         assert_that(self.temp.encrypt('XYZ'), equal_to('UVW'))
+    def test_ceasar_last_3_letters_lowercase(self):
+        assert_that(self.temp.encrypt('xyz'), equal_to('uvw'))
+    def test_ceasar_whole_alphabet(self):
+        assert_that(self.temp.encrypt('ABCDEFGHIJKLMNOPQRSTUVWXYZ'), equal_to('DEFGHIJKLMNOPQRSTUVWXYZUVW'))
+    def test_ceasar_whole_alphabet_lowercase(self):
+        assert_that(self.temp.encrypt('abcdefghijklmnopqrstuvxyz'), equal_to('defghijklmnopqrstuvwxyuvw'))
+    def test_ceaasar_sentence(self):
+        assert_that(self.temp.encrypt('hello world'), equal_to('khoor zruog'))
+
+
+
