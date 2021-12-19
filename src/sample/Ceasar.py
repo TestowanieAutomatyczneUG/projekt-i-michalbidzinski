e12ad23ed6c3402ef89ebd4201e5e12486705c3b
Michał Bidziński <michal.bidzinski1337@gmail.com>
2021-12-20 00:17:25 +0100

    refaktoryzacja funkcji encrypt

--- src/sample/Ceasar.py
+++ src/sample/Ceasar.py
@@ -1,7 +1,13 @@
 class Ceasar:
+    def __init__(self):
+        self.alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     def encrypt(self, text):
-        result = ""
-        if text == "a":
-            return "d"
-        if text == 'VENI':
-            return "YHQL"
+        cipher = ''
+        for i in text:
+            if i.isupper():
+                index = self.alphabet.index(i.lower()) + 3
+                cipher += (self.alphabet[index]).upper()
+            else:
+                index = self.alphabet.index(i.lower()) + 3
+                cipher += (self.alphabet[index])
+        return cipher
