1a91904811e8f76983eeb96264ff1b2354ebf126
Michał Bidziński <michal.bidzinski1337@gmail.com>
2021-12-20 01:37:26 +0100

    dodano funkcje decrypt + 5 testow wszystkie dzialaja

--- src/sample/Ceasar.py
+++ src/sample/Ceasar.py
@@ -28,3 +28,30 @@ class Ceasar:
             else:
                 cipher += ' '
         return cipher
+
+    def decrypt(self, text):
+        decrypted_text = ''
+        if not (isinstance(text, str)):
+            raise Exception
+        for i in text:
+            if i.lower() not in self.alphabet and i != ' ':
+                raise Exception
+
+            if i.isupper() and i != ' ':
+                if self.alphabet.index(i.lower()) <= 3:
+                    index = self.alphabet.index(i.lower()) + 23
+                    decrypted_text += (self.alphabet[index]).upper()
+                else:
+                    index = self.alphabet.index(i.lower()) - 3
+                    decrypted_text += (self.alphabet[index]).upper()
+            elif i.islower() and i != ' ':
+                if self.alphabet.index(i.lower()) <= 3:
+                    index = self.alphabet.index(i.lower()) + 23
+                    decrypted_text += (self.alphabet[index])
+                else:
+                    index = self.alphabet.index(i.lower()) - 3
+                    decrypted_text += (self.alphabet[index])
+            else:
+                decrypted_text += ' '
+        return decrypted_text
+
--- tests/test_ceasar_pyhamcrest.py
+++ tests/test_ceasar_pyhamcrest.py
@@ -4,6 +4,7 @@ from src.sample.Ceasar import Ceasar
 class TestCeasar(unittest.TestCase):
     def setUp(self):
         self.temp = Ceasar()
+    # ecnrypt
     def test_ceasar_letter(self):
         assert_that(self.temp.encrypt('a'), equal_to('d'))
     def test_ceasar_word(self):
@@ -24,6 +25,7 @@ class TestCeasar(unittest.TestCase):
         assert_that(self.temp.encrypt('ala ma kota'), equal_to('dod pd nrwd'))
     def test_ceasar_with_multiple_spaces(self):
         assert_that(self.temp.encrypt('ala     ma   kota'), equal_to('dod     pd   nrwd'))
+    # exceptions
     def test_ceasar_not_in_alphabet(self):
         assert_that(calling(self.temp.encrypt).with_args('2'), raises(Exception))
     def test_ceasar_None(self):
@@ -43,7 +45,23 @@ class TestCeasar(unittest.TestCase):
 
     def test_ceasar_float(self):
         assert_that(calling(self.temp.encrypt).with_args(3.14), raises(Exception))
+    # decrypt
+    def test_ceasar_decrypt_letter(self):
+        assert_that(self.temp.decrypt('a'), equal_to('x'))
 
+    def test_ceasar_decryp_decryp_word(self):
+        assert_that(self.temp.decrypt('VENI'), equal_to('SBKF'))
+
+    def test_ceasar_decryp_word_lowercase(self):
+        assert_that(self.temp.decrypt('veni'), equal_to('sbkf'))
+
+    def test_ceasar_decryp_last_3_letters(self):
+        assert_that(self.temp.decrypt('XYZ'), equal_to('UVW'))
+
+    def test_ceasar_decryp_last_3_letters_lowercase(self):
+        assert_that(self.temp.decrypt('xyz'), equal_to('uvw'))
+
+ 
 
 
 
