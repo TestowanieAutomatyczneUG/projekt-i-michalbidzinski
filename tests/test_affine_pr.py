4e96cd9a4ff4940c943c0a905d525cf6a09174dd
Michał Bidziński <michal.bidzinski1337@gmail.com>
2021-12-20 03:08:50 +0100

    test affine ze literami

--- tests/test_affine_pr.py
+++ tests/test_affine_pr.py
@@ -10,9 +10,21 @@ class Affine_test_parameterized(unittest.TestCase):
         ('VINI', 3, 32, 'RETE'),
         ('dici', 2, 16, 'wgug'),
         ('IL', 5, 15, 'DS'),
-        ('DUCE',25 ,25, 'WFXV' )
+        ('DUCE',25 ,25, 'WFXV' ),
+        ('ABCDEFGHIJKLMNOPQRSTUVWXYZ', 3, 12, 'MPSVYBEHKNQTWZCFILORUXADGJ'),
     ])
 
     def test_affine_words(self, text, a, b, cipher):
+        self.assertEqual(self.temp.affine_encrpyt(text, a, b), cipher)
+
+    @parameterized.expand([
+        ('a', 3, 12, 'm'),
+        ('B', 3, 12, 'P'),
+        ('c', 3, 12, 's'),
+        ('X', 3, 12, 'D'),
+        ('y', 3, 12, 'g'),
+        ('Z', 3, 12, 'J')
+    ])
+    def test_affine_single_letters(self, text, a, b, cipher):
         self.assertEqual(self.temp.affine_encrpyt(text, a, b), cipher)
 
