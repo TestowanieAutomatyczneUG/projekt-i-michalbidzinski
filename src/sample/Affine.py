716230aa93721045cea80e562216188a907a6c1f
Michał Bidziński <michal.bidzinski1337@gmail.com>
2021-12-20 19:35:41 +0100

    dodano testy dla decrypt affine

--- src/sample/Affine.py
+++ src/sample/Affine.py
@@ -22,3 +22,39 @@ class Affine:
             else:
                 cipher += ' '
         return cipher
+
+    def affine_decrpyt(self, text ,a, b ):
+        def egcd(a, b):
+            x, y, u, v = 0, 1, 1, 0
+            while a != 0:
+                q, r = b // a, b % a
+                m, n = x - u * q, y - v * q
+                b, a, x, y, u, v = a, r, u, v, m, n
+            gcd = b
+            return gcd, x, y
+
+        def modinv(a, m):
+            gcd, x, y = egcd(a, m)
+            if gcd != 1:
+                return None
+            else:
+                return x % m
+        a_inv = modinv(a, len(self.alphabet))
+        decrypted_message = ''
+        if not isinstance(a, int) or not isinstance(b, int) or a < 0 or b < 0:
+            raise Exception
+
+        if not (isinstance(text, str)):
+            raise Exception
+        for i in text:
+            if i.lower() not in self.alphabet and i != ' ':
+                raise Exception
+            if i.isupper() and i != ' ':
+                index =  (a_inv  * (self.alphabet.index(i.lower()) - b)) % len(self.alphabet)
+                decrypted_message += (self.alphabet[index]).upper()
+            elif i.islower() and i != ' ':
+                index = (a_inv  * (self.alphabet.index(i) - b)) % len(self.alphabet)
+                decrypted_message += (self.alphabet[index])
+            else:
+                decrypted_message += ' '
+        return decrypted_message
--- tests/test_affine_pr.py
+++ tests/test_affine_pr.py
@@ -64,4 +64,54 @@ class Affine_test_parameterized(unittest.TestCase):
     ])
     def test_affine_wrong_a_or_b(self, text, a, b):
         self.assertRaises(Exception, self.temp.affine_encrpyt, text, a, b)
+    # ------
+    # DECRYPT
+    # -----
+    @parameterized.expand([
+        ('xyzk', 3, 12, 'veni'),
+        ('RETE', 3, 32, 'VINI'),
+        ('DS', 5, 15, 'IL'),
+        ('WFXV', 25, 25, 'DUCE'),
+        ('MPSVYBEHKNQTWZCFILORUXADGJ', 3, 12, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'),
+    ])
+    def test_affine_decrypt_words(self, text, a, b, decrypted_message):
+        self.assertEqual(self.temp.affine_decrpyt(text, a, b), decrypted_message)
+
+    @parameterized.expand([
+        ('hyttc acltv', 3, 12, 'hello world'),
+        ('akrmn oakysky', 3, 12, 'witaj swiecie'),
+        ('AYOCTGSH OAKMR', 3, 12, 'WESOLYCH SWIAT'),
+
+    ])
+    def test_affine_decrypt_sentences(self, text, a, b, cipher):
+        self.assertEqual(self.temp.affine_decrpyt(text, a, b), cipher)
+
+    @parameterized.expand([
+        ('123', 3, 12),
+        (',&!2@;.xxxz', 3, 12),
+        (1, 3, 12),
+        (str, 3, 12),
+        ([], 3, 12),
+        ({}, 3, 12),
+        (None, 3, 12 ),
+        (True, 3, 12 ),
+        ("ĄĄĆGF", 3, 12)
+    ])
+    def test_affine_decrypt_wrong_cipher_provided(self, text, a, b):
+        self.assertRaises(Exception, self.temp.affine_decrpyt, text, a, b)
+    @parameterized.expand([
+        ('hello world', 'x', 12),
+        ('witaj swiecie', 'd', 12),
+        ('witaj swiecie', -3, 12),
+        ('witaj swiecie', [], 12),
+        ('witaj swiecie', 3, 'x'),
+        ('witaj swiecie', 3, 'd'),
+        ('witaj swiecie', 3, -3),
+        ('witaj swiecie', 3, []),
+        ('witaj swiecie', -3, -3),
+        ('witaj swiecie', -1, -2),
+    ])
+    def test_affine_decypher_wrong_a_or_b(self, text, a, b):
+        self.assertRaises(Exception, self.temp.affine_decrpyt, text, a, b)
+
 
