5b9a5945c8b83c25bcde185ecd4feb47ef82dc02
Michał Bidziński <michal.bidzinski1337@gmail.com>
2021-12-21 18:48:24 +0100

    przeniesienie doctestu do pliku Ceasar

--- src/sample/Ceasar.py
+++ src/sample/Ceasar.py
@@ -4,6 +4,46 @@ class Ceasar:
                          't', 'u', 'v', 'w', 'x', 'y', 'z', ]
 
     def encrypt(self, text):
+        """
+             >>> g = Ceasar()
+             >>> g.encrypt('a')
+             'd'
+             >>> g.encrypt('VENI')
+             'YHQL'
+             >>> g.encrypt('XYZ')
+             'ABC'
+             >>> g.encrypt('xyz')
+             'abc'
+             >>> g.encrypt('abcdefghijklmnopqrstuvxyz')
+             'defghijklmnopqrstuvwxyabc'
+
+             >>> g.encrypt('hello world')
+             'khoor zruog'
+             >>> g.encrypt('ala ma kota')
+             'dod pd nrwd'
+             >>> g.encrypt('2')
+             Traceback (most recent call last):
+             ...
+             Exception
+             >>> g.encrypt('b')
+             'e'
+             >>> g.encrypt([])
+             Traceback (most recent call last):
+             ...
+             Exception
+             >>> g.encrypt(3.14)
+             Traceback (most recent call last):
+             ...
+             Exception
+             >>> g.encrypt(2)
+             Traceback (most recent call last):
+             ...
+             Exception
+
+
+
+
+             """
         cipher = ''
         if not (isinstance(text, str)):
             raise Exception
@@ -30,6 +70,42 @@ class Ceasar:
         return cipher
 
     def decrypt(self, text):
+        """
+                    >>> g = Ceasar()
+                    >>> g.decrypt('a')
+                    'x'
+                    >>> g.decrypt('VENI')
+                    'SBKF'
+                    >>> g.decrypt('XYZ')
+                    'UVW'
+
+                    >>> g.decrypt('abcdefghijklmnopqrstuvxyz')
+                    'xyzabcdefghijklmnopqrsuvw'
+
+                    >>> g.decrypt('hello world')
+                    'ebiil tloia'
+                    >>> g.decrypt('ala ma kota')
+                    'xix jx hlqx'
+                    >>> g.decrypt('2')
+                    Traceback (most recent call last):
+                    ...
+                    Exception
+                    >>> g.decrypt('b')
+                    'y'
+                    >>> g.decrypt([])
+                    Traceback (most recent call last):
+                    ...
+                    Exception
+                    >>> g.decrypt(3.14)
+                    Traceback (most recent call last):
+                    ...
+                    Exception
+                    >>> g.decrypt(2)
+                    Traceback (most recent call last):
+                    ...
+                    Exception
+
+                    """
         decrypted_text = ''
         if not (isinstance(text, str)):
             raise Exception
