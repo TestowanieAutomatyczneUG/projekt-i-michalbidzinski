ed6dd9de2b535b16ae4f4bad4dafa5af9d762e50
Michał Bidziński <michal.bidzinski1337@gmail.com>
2021-12-21 12:22:41 +0100

    dodanie testu dla wyjatkow z pliku json

--- tests/test_ceasar_pr_from_file.py
+++ tests/test_ceasar_pr_from_file.py
@@ -18,27 +18,3 @@ class CeasarTestFromFile(unittest.TestCase):
         testData = json.loads(jsonFile.read())
         for input in testData:
             self.assertRaises(Exception, ceasar.encrypt, input)
-    def test_Ceasar_encrypt_from_file_word(self):
-        # data = open("../data/data")
-        data = open("C:/Users/mbidz/PycharmProjects/projekt-i-michalbidzinski/data/data")
-        ceasar = Ceasar()
-        for lines in data:
-            line = lines.split(" ")
-            input, output = line[0], line[1].strip("\n")
-            self.assertEqual(ceasar.encrypt(input), output)
-    def test_Ceasar_encrypt_from_file_sentence_with_spaces(self):
-        # data = open("../data/dataWithSpaces")
-        data = open("C:/Users/mbidz/PycharmProjects/projekt-i-michalbidzinski/data/dataWithSpaces")
-        ceasar = Ceasar()
-        for lines in data:
-            line = lines.split(" ")
-            input, output = line[0] + " " +  line[1], line[2].strip("\n") + " " +  line[3].strip("\n")
-            self.assertEqual(ceasar.encrypt(input), output)
-    def test_Ceasar_encrypt_from_file_word_exceptions(self):
-        # data = open("../data/dataExceptions")
-        data = open("C:/Users/mbidz/PycharmProjects/projekt-i-michalbidzinski/data/dataExceptions")
-        ceasar = Ceasar()
-        for lines in data:
-            line = lines.split(" ")
-            input = line[0]
-            self.assertRaises(Exception, ceasar.encrypt, input)
